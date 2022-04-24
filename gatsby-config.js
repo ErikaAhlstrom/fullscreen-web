4eee9d99e4dea7da088fe0fb059f82a075390ae8
Erika Ahlström <info@erikasoldh.se>
2022-04-24 20:03:59 +0200

    fix: add plugins to gatsby config file

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,10 +1,43 @@
-/**
- * Configure your Gatsby site with this file.
- *
- * See: https://www.gatsbyjs.com/docs/gatsby-config/
- */
+
+/* require('dotenv').config({
+  path: `.env.${process.env.NODE_ENV}`, 
+}); */
 
 module.exports = {
-  /* Your site config here */
-  plugins: [],
+  siteMetadata: {
+    title: 'Fullscreen',
+    description: 'Fullscreen AB är ett nystartat produktionsbolag med fokus på dokumentärt berättande.',
+  },
+  plugins: [
+   
+    `gatsby-plugin-react-helmet`,
+    `gatsby-plugin-styled-components`,
+    `gatsby-plugin-image`,
+    `gatsby-plugin-sharp`,
+    `gatsby-transformer-sharp`,
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `images`,
+        path: `${__dirname}/src/assets/images`,
+      },
+    },
+    /* {
+      resolve: `gatsby-source-contentful`,
+      options: {
+        spaceId: process.env.CONTENTFUL_SPACE_ID,
+        accessToken: process.env.CONTENTFUL_ACCESS_TOKEN,
+      },
+    }, */
+    {
+      resolve: 'gatsby-plugin-web-font-loader',
+      options: {
+        google: {
+          families: ['Sacramento', 'Outfit Sans:100, 200, 300, 400, 500, 600, 700, 800, 900']
+        }
+      }
+    },
+    
+    
+  ],
 }
